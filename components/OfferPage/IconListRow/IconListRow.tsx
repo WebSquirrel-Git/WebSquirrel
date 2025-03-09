56ee3c94aaf0c180ee6bbd7435a64d01b446840d
Bartosz <104717320+bartosz-bednarski@users.noreply.github.com>
2025-03-09 12:55:36 +0100

    Dev (#6)
    * Created footer.
    
    * Implemented routing for offer.
    
    * Update.
    
    * Created ProcessSteps and IconListRow components.

---
+++ components/OfferPage/IconListRow/IconListRow.tsx
@@ -0,0 +1,36 @@
+'use client';
+import styles from './iconListRow.module.scss';
+import ReaderIcon from '@/public/assets/icons/reader-icon.svg';
+import CheckmarkDoneCircleIcon from '@/public/assets/icons/checkmark-done-circle-icon.svg';
+import CloseCircleIcon from '@/public/assets/icons/close-circle-icon.svg';
+import CodeSlashIcon from '@/public/assets/icons/code-slash-icon.svg';
+
+const ICONS = {
+  reader: ReaderIcon,
+  checkmarkDone: CheckmarkDoneCircleIcon,
+  closeCircle: CloseCircleIcon,
+  codeSlash: CodeSlashIcon,
+};
+
+export interface IconListRowPropsType {
+  list: string[];
+  icon: 'reader' | 'checkmarkDone' | 'closeCircle' | 'codeSlash';
+  title: string;
+}
+
+const IconListRow = ({list, icon, title}: IconListRowPropsType) => {
+  const img = ICONS[icon];
+  return (
+    <div className={styles.container}>
+      <img src={img.src} className={styles.icon} />
+      <div className={styles.contentBox}>
+        <h3>{title}</h3>
+        {list.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default IconListRow;
